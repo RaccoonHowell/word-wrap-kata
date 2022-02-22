619f200167cea9a49e893200494c27673810b3e2
Rachel Howell <rachel.howell@madetech.com>
2022-02-22 12:10:38 +0000

    fail test for checking whether string is longer than number

--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -6,10 +6,12 @@ describe Wrapper do
     end
 
     it "can return a string that is passed into the function" do
-        result = @wrapper.wrap(hi)
+        result = @wrapper.wrap("hi")
         expect(result).to eq("hi")
     end
 
-    # it "can return a string nd number that are passed into the function" do
-    #     result = @wrapper.wrap()
+    it "can check whether string is longer than number" do
+        result = @wrapper.wrap("hi", 7)
+        expect(result).to eq(false)
+    end
 end
