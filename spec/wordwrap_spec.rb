bd1d8959f09ecad8450f4807a564db99c292dfd3
Rachel Howell <rachel.howell@madetech.com>
2022-02-22 12:24:44 +0000

    fail test for returning a shortened string containing only (number) characters

--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -9,7 +9,12 @@ describe Wrapper do
         result = @wrapper.wrap("hi", 7)
         expect(result).to eq(false)
 
-        result2 = @wrapper.wrap("helloooo", 5)
-        expect(result2).to eq(true)
+        result = @wrapper.wrap("hi", 1)
+        expect(result).to eq(true)
+    end
+
+    it "can return a shortened string containing only (number) characters" do
+        result = @wrapper.wrap("hello world", 2)
+        expect(result). to eq("he")
     end
 end
