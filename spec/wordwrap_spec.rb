6c1c004265bc68b6c43474499f08bff9b58dba44
Rachel Howell <rachel.howell@madetech.com>
2022-02-22 12:13:27 +0000

    pass test for checking whether string is longer than number

--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -8,5 +8,8 @@ describe Wrapper do
     it "can check whether string is longer than number" do
         result = @wrapper.wrap("hi", 7)
         expect(result).to eq(false)
+
+        result2 = @wrapper.wrap("helloooo", 5)
+        expect(result2).to eq(true)
     end
 end
