a346cf43c2a3cd2c7a8cdf6f6db21c9e20047927
Rachel Howell <rachel.howell@madetech.com>
2022-02-22 11:56:58 +0000

    pass test for returning a string that is passed into the function

--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -5,13 +5,8 @@ describe Wrapper do
         @wrapper = described_class.new
     end
 
-    it "can return a string" do
-        result = @wrapper.wrap
-        expect(result).to eq('hello')
-    end
-
     it "can return a string that is passed into the function" do
-        esult = @wrapper.wrap('hi')
-        expect(result).to eq('hi')
+        result = @wrapper.wrap("hi")
+        expect(result).to eq("hi")
     end
 end
